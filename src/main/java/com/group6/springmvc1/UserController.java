55b2bd93083eee3a634a4e522be030822aa6a383
RigHakiko <add-122@hotmail.com>
2022-10-11 18:24:40 +0900

    终于成功运行了controller的传值

--- src/main/java/com/group6/springmvc1/UserController.java
+++ src/main/java/com/group6/springmvc1/UserController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @RequestMapping(value = "/aaa")
-    public void login(){
+    public void login(String username, String password){
+        System.out.println(username);
+        System.out.println(password);
         System.out.println("hello, 进入到方法内部了");
     }
 }
