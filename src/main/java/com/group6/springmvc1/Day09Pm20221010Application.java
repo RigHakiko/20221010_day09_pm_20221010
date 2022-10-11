43a8ffb3d797457dbd956ff2dc4eccbb33557e26
RigHakiko <add-122@hotmail.com>
2022-10-11 17:37:57 +0900

    成功地运行了第1个页面

--- src/main/java/com/group6/springmvc1/Day09Pm20221010Application.java
+++ src/main/java/com/group6/springmvc1/Day09Pm20221010Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Day09Pm20221010Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day09Pm20221010Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Day09Pm20221010Application.class, args);
+    }
 
 }
