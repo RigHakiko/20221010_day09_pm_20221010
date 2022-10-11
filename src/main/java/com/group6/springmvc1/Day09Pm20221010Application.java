5264aaa144ee85130830c183b269085faa220c73
RigHakiko <add-122@hotmail.com>
2022-10-11 17:37:27 +0900

    Initial commit

---
+++ src/main/java/com/group6/springmvc1/Day09Pm20221010Application.java
@@ -0,0 +1,13 @@
+package com.group6.springmvc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day09Pm20221010Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day09Pm20221010Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/group6/springmvc1/Day09Pm20221010ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group6.springmvc1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day09Pm20221010ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
